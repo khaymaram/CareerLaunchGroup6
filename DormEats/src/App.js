817d4dbb18562c6cc24801d7ab2afc886bc8b77f
khaymaram <kamoe25@terpmail.umd.edu>
2025-01-16 15:53:28 -0500

    Changed name to DormEats

---
+++ DormEats/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={'https://assets.zenn.com/strapi_assets/food_logo_5fbb88038c.png'} className="App-logo" alt="logo" />
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        <p>Welcome to DormEats</p>
+        <a
+          className="App-link"
+          href="https://www.allrecipes.com/gallery/cheap-and-easy-meals-for-college-students/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Let's Cook!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
