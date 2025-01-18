33e0da28e58f9e7a4f3d7db0823e0bb81c70a991
Khaymar Moe <kamoe25@terpmail.umd.edu>
2025-01-18 17:31:09 -0500

    Edited HomePage to have recipe block reflect database

--- DormEats_v1/src/components/HomePage.jsx
+++ DormEats_v1/src/components/HomePage.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from "react-router-dom";
 import './HomePage.css'
+import supabase from '../supabase-client';
 
 const HomePage = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+      getRecipes();
+    }, []);
+  
+    async function getRecipes() {
+      const { data } = await supabase.from("Recipes").select();
+      setRecipes(data);
+    }
+
   return (
     <div className='HomePage'>
       <header className='HomePage-header'>
@@ -58,10 +70,17 @@ const HomePage = () => {
 
         <div className='HomePage-recipes'>
           <h2 className='HomePage-subheading'>Recipes</h2>
-          <p className='HomePage-recipeBlock'>Recipe 1</p>
-          <Link to="/recipe" >Go to Recipe</Link>
-          <p className='HomePage-recipeBlock'>Recipe 2</p>
-          <p className='HomePage-recipeBlock'>Recipe 3</p>
+          <ul>
+            {recipes.map((recipe) => (
+                <li key={recipe.id} className='HomePage-recipeBlock'>
+                  <strong>{recipe.name}</strong>
+                  <div>
+                      {recipe.ingredients.join(", ")}
+                  </div>
+                </li>
+            ))}
+          </ul>
+          <Link to="/recipe" >Go to Recipe</Link>        
         </div>
       </div>
     </div>
