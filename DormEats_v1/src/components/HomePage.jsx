49ff7aa2c6aea6a777f67bb51427d86e42db27e9
Khaymar Moe <kamoe25@terpmail.umd.edu>
2025-01-18 18:54:02 -0500

    Adjusted boxes so headings would stay at the top even when the box grows

--- DormEats_v1/src/components/HomePage.jsx
+++ DormEats_v1/src/components/HomePage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
   return (
     <div className='HomePage'>
       <header className='HomePage-header'>
-        <h1>DormEats</h1>
+        <h1>Dorm Eats</h1>
         <img src='https://cdn-icons-png.flaticon.com/512/8382/8382732.png' width= '75px' height = '75px' alt='logo'/>
       </header>
 
